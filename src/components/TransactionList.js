9d1554c4d28f659a88352079e66ff46872e570f1
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-01-15 15:40:41 +0300

    coding

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import Transaction from './Transaction';
+
+function TransactionsList({ transactions, handleDeleteTransaction, handleAddForm}) {
+
+    const deleteTransaction = async (transId) => {
+          console.log(transId);
+          try {
+              const res = await fetch("http://localhost:8001/transactions/" + transId, {
+                  method: "DELETE",
+              });
+              handleDeleteTransaction(transId);
+          } catch (error) {
+              console.log(error);
+          }
+      };
+    
